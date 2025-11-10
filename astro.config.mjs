7358ba8660cbd19d3b9da2638eaef4756af13d6e
nehu3n <developer.nh3@gmail.com>
2025-11-09 21:15:29 -0300

    fix: github social starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,25 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config";
 
-import starlight from '@astrojs/starlight'
+import starlight from "@astrojs/starlight";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()]
-	},
-	integrations: [
-		starlight({
-			title: 'Kito',
-			description: '🐺 The high-performance, type-safe and modern TypeScript web framework written in Rust.',
-			favicon: '/favicon.png',
-			social: {
-				github: 'https://github.com/kitojs/kito'
-			}
-		}),
-	],
-})
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [
+    starlight({
+      title: "Kito",
+      description:
+        "🐺 The high-performance, type-safe and modern TypeScript web framework written in Rust.",
+      favicon: "/favicon.png",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/kitojs/kito",
+        },
+      ],
+    }),
+  ],
+});
