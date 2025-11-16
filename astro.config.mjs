e057fe0c76252789e87908360ef78f2d13b61633
nehu3n <developer.nh3@gmail.com>
2025-11-16 03:09:19 -0300

    chore: add vite public dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    publicDir: "public",
   },
   integrations: [
     starlight({
